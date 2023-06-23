21d712bce9376a879acaa470c1ed046545995b8b
Aman Anand Tirpude <79367645+amantirpude@users.noreply.github.com>
2023-06-24 00:26:17 +0530

    Update LargestNumber.java

--- LargestNumber.java
+++ LargestNumber.java
@@ -1,20 +1,36 @@
 import java.util.Scanner;
 
-public class LargestNumber {
-    public static int largest(int[] arr){
-        int maxi = Integer.MIN_VALUE;
+public class SecondSmallLarge {
+    public static void secondSmallLarge(int[] arr){
+        int small = Integer.MAX_VALUE;
+        int large = Integer.MIN_VALUE;
+        int second_small = Integer.MAX_VALUE;
+        int second_large = Integer.MIN_VALUE;
         for(int i =0; i<arr.length; i++){
-            maxi = Math.max(maxi,arr[i]);
+            if(arr[i] < small){
+                second_small = small;
+                small = arr[i];
+            }
+            else if(arr[i] < second_small && arr[i] != small){
+                second_small = arr[i];
+            }
+            if(arr[i] > large){
+                second_large = large;
+                large = arr[i];
+            }
+            else if(arr[i] >second_large && arr[i] != large){
+                second_large = arr[i];
+            }
         }
-        return maxi;
+        System.out.println(second_small + " " + second_large);
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
         for(int i =0; i<n; i++){
-          arr[i] = sc.nextInt();
+            arr[i] = sc.nextInt();
         }
-        System.out.println(LargestNumber.largest(arr));
+        SecondSmallLarge.secondSmallLarge(arr);
     }
 }
